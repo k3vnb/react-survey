c95e20907f2bf4c8426d8d1a91503356f7f4cdb7
Kevin Boyle <papershack@gmail.com>
2018-10-23 17:19:19 -0700

    fix middleware bug

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,7 @@
 //decides whether or not user is logged in
 
 export default function(state = {}, action){
+    console.log(action);
     switch(action.type){
         default: 
             return state;
