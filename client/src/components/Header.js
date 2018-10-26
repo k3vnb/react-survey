a74832624f5c46b37e161ad437ce88401622e929
Kevin Boyle <papershack@gmail.com>
2018-10-25 23:47:30 -0700

    implement redirect from brand logo in header conditional on if user is logged in

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'; 
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
     renderContent(){
@@ -18,7 +19,11 @@ class Header extends Component {
         return (
             <nav>
             <div className="nav-wrapper">
-              <a href="#" className="left brand-logo">SurveyMania</a>
+              <Link to={this.props.auth ? '/surveys' : '/'} 
+              className="left brand-logo"
+              >
+                SurveyMania
+              </Link>
               <ul id="nav-mobile" className="right">
                 {this.renderContent()}
               </ul>
