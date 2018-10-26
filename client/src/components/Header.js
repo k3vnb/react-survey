2a622b6e716c44825dbd3bc99ae19257a231427d
Kevin Boyle <papershack@gmail.com>
2018-10-25 23:16:00 -0700

    added rendering based on state in header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'; 
 
 class Header extends Component {
+    renderContent(){
+      switch (this.props.auth) {
+        case null: 
+          return 'Still deciding';
+        case false:
+          return 'im logged out';
+        default:
+          return 'im logged in';
+      }
+    }
     render() {
         return (
             <nav>
             <div className="nav-wrapper">
               <a href="#" className="left brand-logo">SurveyMania</a>
-              <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="#">Login with Google</a></li>
+              <ul id="nav-mobile" className="right">
+                {this.renderContent()}
               </ul>
             </div>
           </nav>
         );
     }
 }
-
-export default Header;
+function mapStateToProps({ auth }){
+  return { auth };
+}
+export default connect(mapStateToProps) (Header);
