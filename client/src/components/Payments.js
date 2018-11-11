f80c8c714e8d5d9efed0b28849e4f5e5a64d034a
Kevin Boyle <papershack@gmail.com>
2018-11-10 17:28:45 -0800

    complete installation of stripe wrapper, it visually displays on screen now

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -3,12 +3,20 @@ import StripeCheckout from 'react-stripe-checkout';
 
 class Payments extends Component {
     render(){
+        debugger;
+
         return (
-            <StripeCheckout 
+            <StripeCheckout
+                name="SurveyMania"
+                description="$2 for a survey" 
                 amount={200}
                 token={token => console.log(token)}
                 stripeKey={process.env.REACT_APP_STRIPE_KEY}
-            />
+            >
+                <button className="btn">Add Money</button>
+            </StripeCheckout>
         )
     }
 }
+
+export default Payments;
