aaf2162163bdec77ab94e63b68b87e736e5ba918
Kevin Boyle <papershack@gmail.com>
2018-11-11 23:00:03 -0800

    fix typo

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react'
 import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class Payments extends Component {
     render(){
-        debugger;
+
 
         return (
             <StripeCheckout
                 name="SurveyMania"
                 description="$2 for a survey" 
                 amount={200}
-                token={token => console.log(token)}
+                token={token => this.props.handleToken(token)}
                 stripeKey={process.env.REACT_APP_STRIPE_KEY}
             >
                 <button className="btn">Add Money</button>
@@ -19,4 +21,4 @@ class Payments extends Component {
     }
 }
 
-export default Payments;
+export default connect(null, actions) (Payments);
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const keys = require('./config/keys');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 require('./models/user');
 require('./services/passport');
 
@@ -10,6 +11,7 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
     cookieSession({
         //maxAge, how long cookie is valid in browser, expressed in milliseconds
@@ -22,6 +24,7 @@ app.use(passport.session());
 
 // our authRoutes file returns a function, here we pass app to it as an arg. 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 
 const PORT = process.env.PORT || 5000;
