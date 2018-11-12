cabf900c6daf1f9047e2db5ae22312ba61bfd3ff
Kevin Boyle <papershack@gmail.com>
2018-11-11 23:14:12 -0800

    adjust billingRoutes to check for user log in state, updating credits in database

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,13 +3,21 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
     app.post('/api/stripe', async (req, res) => {
-        // const charge = await stripe.charges.create({
-        //     amount: 200,
-        //     currency: 'usd',
-        //     description: '$2 for a Survey',
-        //     source: req.body.id
-        // });
+        if (!req.user){
+            return res.status(401).send({ error: 'You must log in' });
+        }
+        const charge = await stripe.charges.create({
+            amount: 200,
+            currency: 'usd',
+            description: '$2 for a Survey',
+            source: req.body.id
+        });
         // console.log(keys.stripeSecretKey, charge);
-        console.log(req.body);
+        // console.log(req.body)
+        req.user.credits += 2;
+        const user = await req.user.save();
+
+        res.send(user);
+    
     });
 };
