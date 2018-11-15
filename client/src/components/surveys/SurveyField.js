395082588564e20ef10b9ad1b19265adadf5f6ff
Kevin Boyle <papershack@gmail.com>
2018-11-14 18:49:43 -0800

    set up form structure w/ redux-formand create loop for form fields

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,14 @@
+//SurveyField contains logic to render a single label and text input
+
+import React from 'react';
+
+export default ({ input, label }) => {
+    // console.log(input); 
+    //input is props.input
+    return (
+        <div>
+            <label>{label}</label>
+            <input {...input} />
+        </div>
+    )
+};
