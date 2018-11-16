9715a8fae7de38d97cfbae9759535ea39b396692
Kevin Boyle <papershack@gmail.com>
2018-11-15 22:04:21 -0800

    set up form validation, and user can click to begin review of entries

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -2,7 +2,9 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
+import validateEmails from '../../utils/validateEmails';
 
 const FIELDS = [
     {label: 'Survey Title', name: 'title'},
@@ -22,16 +24,34 @@ class SurveyForm extends Component {
     render() {
         return (
             <div>
-                <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+                <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
                     {this.renderFields()}
-                        <button type="submit">Submit</button>
+                    <Link to="/surveys" className="red btn-flat white-text">
+                        Cancel
+                    </Link>
+                    <button className="teal btn-flat right white-text" type="submit">
+                        Submit
+                        <i className="material-icons right">done</i>
+                    </button>
                 </form>
             </div>
         );
     }
 }
 
+function validate(values){
+    const errors = {};
+    errors.emails = validateEmails(values.emails || '');
+
+    _.each(FIELDS, ({ name }) => {
+        if (!values[name]) {
+            errors[name] = 'You must fill out this field' ;   
+        }
+    });
+    return errors;
+}
 export default reduxForm({
+    validate,
     form: 'surveyForm'
 })(SurveyForm);
 
