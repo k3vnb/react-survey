5eceffc06986ac59594cc0f79b5fe103e68e095e
Kevin Boyle <papershack@gmail.com>
2018-11-16 16:08:15 -0800

    complete chain of functions to send email after form is verified, redirect user to surveys page

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -3,8 +3,10 @@ import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
+import { withRouter } from 'react-router-dom';
+import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
     const reviewFields = _.map(formFields, ({ name, label})=> {
         return (
             <div key={name}>
@@ -20,10 +22,17 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
             <h5>Please confirm your entries</h5>
             {reviewFields}
             <button 
-                className="yellow darken-3 btn-flat"
+                className="yellow darken-3 white-text btn-flat"
                 onClick={ onCancel }
             >
             Back
+            </button>
+            <button 
+                className="green btn-flat white-text right"
+                onClick={() => submitSurvey(formValues, history)}
+                >
+               Send Survey
+               <i className="material-icons right">email</i> 
             </button>
         </div>
     );
@@ -36,4 +45,4 @@ function mapStateToProps(state){
     };
 }
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,4 +1,6 @@
+//SurveyNew is the parent of SurveyForm and SurveyFormReview
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 import SurveyForm from './SurveyForm';
 import SurveyFormReview from './SurveyFormReview';
 
@@ -27,4 +29,7 @@ class SurveyNew extends Component {
     }
 }
 
-export default SurveyNew;
+//using reduxForm helper here means if user navigates away from SurveyNew, form will be cleared
+export default reduxForm({
+    form: 'surveyForm'
+})(SurveyNew);
