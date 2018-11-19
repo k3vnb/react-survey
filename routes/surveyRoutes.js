a8e8fd6de608b7a1090fc51d6d4d6815ae61b7d0
Kevin Boyle <papershack@gmail.com>
2018-11-19 00:19:21 -0800

    fix bug w/ path-parser, webhook now works correctly in development

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const Path = require('path-parser').default;
+const { URL } = require('url');
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
@@ -7,14 +10,26 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
-  app.get("/api/surveys/thanks", (req, res) => {
-    res.send("Thanks for the feedback!");
-  });
+//   app.get("/api/surveys/thanks", (req, res) => {
+//     res.send("Thanks for the feedback!");
+//   });
 
   app.post("/api/surveys/webhooks", (req, res) => {
-    console.log(req.body);
+    const p = new Path('/api/surveys/:surveyId/:choice');
+    
+    const events = _.map(req.body, ({ email, url }) => {
+      const match = p.test(new URL(url).pathname);
+      if (match){
+        return { email, surveyId: match.surveyId, choice: match.choice };
+      }
+    });
+
+    const compactEvents = _.compact(events);
+    const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+    console.log(uniqueEvents);
     res.send({});
   });
+
   app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
