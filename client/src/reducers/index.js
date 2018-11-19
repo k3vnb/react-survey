9230ee61bc901b580fcad2b3422c8b5d11df4d66
Kevin Boyle <papershack@gmail.com>
2018-11-19 14:30:10 -0800

    create Action for rendering survey lists of users

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { reducer as reduxForm } from 'redux-form';
 import authReducer from './authReducer';
+import surveysReducer from './surveysReducer';
 
 export default combineReducers({
     //the auth piece of state is being managed by authReducer
     auth: authReducer,
-    form: reduxForm
+    form: reduxForm,
+    surveys: surveysReducer
 });
